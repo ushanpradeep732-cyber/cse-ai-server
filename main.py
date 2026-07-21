8d0bb04ec391f8de97347d186d7356c2a392ae1a
Ushan Pradeep cse <ushanpradeep732@gmail.com>
2026-07-21 09:14:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import requests
-import json
+import os
 from datetime import datetime
+import requests
 
 print("===================================")
 print("CSE AI Auto Sync Started")
 print("Time :", datetime.now())
 print("===================================")
-
-import os
 
 SUPABASE_URL = os.environ["SUPABASE_URL"]
 SUPABASE_KEY = os.environ["SUPABASE_KEY"]
@@ -17,18 +15,31 @@ HEADERS = {
     "Authorization": f"Bearer {SUPABASE_KEY}",
     "Content-Type": "application/json"
 }
-def test_supabase():
 
+def test_supabase():
     url = SUPABASE_URL + "/rest/v1/stocks?select=*"
 
-    response = requests.get(
-        url,
-        headers=HEADERS
-    )
+    response = requests.get(url, headers=HEADERS)
 
-    print("Status :", response.status_code)
-    print(response.text)
+    print("Supabase Status :", response.status_code)
 
+    if response.status_code == 200:
+        print("Supabase Connected")
+    else:
+        print(response.text)
+
+def test_cse():
+    url = "https://www.cse.lk/"
+
+    response = requests.get(url, timeout=30)
+
+    print("CSE Status :", response.status_code)
+
+    if response.status_code == 200:
+        print("CSE Website Connected")
+    else:
+        print("Cannot connect to CSE")
 
 if __name__ == "__main__":
     test_supabase()
+    test_cse()
