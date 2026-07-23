d8b2d2ff75138cd63d5244957a24ed6b714b3a73
Ushan Pradeep cse <ushanpradeep732@gmail.com>
2026-07-23 15:11:00 +0530

    Update cse_test.py

--- cse_test.py
+++ cse_test.py
@@ -1,33 +1,17 @@
 import requests
-import json
-
-symbol = "JKH.N0000"
 
 url = "https://www.cse.lk/api/companyInfoSummery"
 
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-}
-
-payload = {
-    "symbol": symbol
-}
-
-
 response = requests.post(
     url,
-    headers=headers,
-    data={
+    params={
         "symbol": "JKH.N0000"
+    },
+    headers={
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json"
     }
 )
-print("Status :", response.status_code)
-print("=" * 80)
 
-try:
-    data = response.json()
-    print(json.dumps(data, indent=2))
-except Exception:
-    print(response.text)
+print("Status :", response.status_code)
+print(response.text)
