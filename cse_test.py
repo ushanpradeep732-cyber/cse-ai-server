dfa250c576dca53bafe1c7371b5b83765596a24f
Ushan Pradeep cse <ushanpradeep732@gmail.com>
2026-07-23 14:29:36 +0530

    Update cse_test.py

--- cse_test.py
+++ cse_test.py
@@ -1,54 +1,23 @@
 import requests
 import json
 
-url = "https://www.cse.lk/api/todaySharePrice?page=0&size=500"
+url = "https://www.cse.lk/api/allSecurityCode"
 
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
-
-print("===================================")
-print("CSE API TEST")
-print("===================================")
-
-response = requests.post(url, headers=headers)
+response = requests.post(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    }
+)
 
 print("Status :", response.status_code)
 
-if response.status_code != 200:
-    print(response.text)
-    exit()
-
-try:
+if response.status_code == 200:
     data = response.json()
-except Exception as e:
-    print("JSON Error :", e)
-    print(response.text[:1000])
-    exit()
 
-print("\nResponse Type :", type(data))
-
-if isinstance(data, list):
     print("Records :", len(data))
+    print()
 
-    if len(data) > 0:
-        print("\nFirst Record:")
-        print(json.dumps(data[0], indent=4))
-
-elif isinstance(data, dict):
-
-    print("\nKeys:")
-    print(list(data.keys()))
-
-    if "content" in data:
-        print("\nContent Records :", len(data["content"]))
-
-        if len(data["content"]) > 0:
-            print("\nFirst Record:")
-            print(json.dumps(data["content"][0], indent=4))
+    print(json.dumps(data[:5], indent=2))
 else:
-    print(data)
-
-print("===================================")
-print("Finished")
-print("===================================")
+    print(response.text)
