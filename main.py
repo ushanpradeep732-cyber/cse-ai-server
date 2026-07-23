9755f451a27e9d7b062c3e1e7add559e0efc92e8
Ushan Pradeep cse <ushanpradeep732@gmail.com>
2026-07-23 23:12:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -74,14 +74,14 @@ symbols = sorted(stock_map.keys())
 
 print("Companies :", len(symbols))
 
+
 for symbol in symbols:
 
     if not symbol.endswith(".N0000"):
-    print("Skipping :", symbol)
-    continue
+        print("Skipping :", symbol)
+        continue
 
     print("Processing :", symbol)
-
     response = requests.post(
         f"https://www.cse.lk/api/companyInfoSummery?symbol={symbol}",
         headers={
